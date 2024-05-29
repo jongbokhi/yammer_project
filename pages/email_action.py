b4818d0e18023c59d82128ff665dc351d769c60b
Jongbok <105177081+jongbokhi@users.noreply.github.com>
2024-05-29 23:08:34 +0200

    Update email_action.py

--- pages/email_action.py
+++ pages/email_action.py
@@ -10,7 +10,7 @@ import seaborn as sns
 dash.register_page(__name__, path='/email_action', name="4️⃣Email Action")
 
 # Read the data
-email_action = pd.read_csv(r'email_action.csv')
+email_action = pd.read_csv('email_action.csv')
 
 open_ct_rate = pd.read_csv('open_ct_rate.csv')
 
